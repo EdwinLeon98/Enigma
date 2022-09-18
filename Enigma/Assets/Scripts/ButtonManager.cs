2aa6c2f259735269418736676e50b325df9d6446
Lavanya Narasimhan <lnarasim@usc.edu>
2022-09-17 18:06:54 -0700

    Added Start Screen

---
+++ Enigma/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    
+    public void ButtonMoveScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
