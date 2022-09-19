4a7fe07020229a019029b6961e2b1962c5f18edb
EdwinLeon98 <edwinleon98@gmail.com>
2022-09-19 13:00:33 -0700

    Have to fix the positioning of the laser

--- Enigma/Assets/Scripts/LaserBeam.cs
+++ Enigma/Assets/Scripts/LaserBeam.cs
@@ -8,34 +8,38 @@ public class LaserBeam{
     GameObject laserObj;
     LineRenderer laser;
     List<Vector3> laserIndices = new List<Vector3>();
-    public int endpoint;
+    Dictionary<string, float> refractiveMaterials = new Dictionary<string, float> {
+        {"Air", 1.0f},
+        {"Glass", 1.5f}
+    };
+    public bool endpoint;
 
-    public LaserBeam(Vector3 pos, Vector3 dir, Material material){
+    public LaserBeam(Vector3 pos, Vector3 dir, Material material, string name, Color color, bool endpoint) {
         this.laser = new LineRenderer();
         this.laserObj = new GameObject();
-        this.laserObj.name = "Laser Beam";
+        this.laserObj.name = name;
         this.pos = pos;
         this.dir = dir;
-        this.endpoint = 0;
+        this.endpoint = endpoint;
 
         this.laser = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
         this.laser.startWidth = 0.08f;
         this.laser.endWidth = 0.08f;
         this.laser.material = material;
-        this.laser.startColor = Color.red;
-        this.laser.endColor = Color.red;
+        this.laser.startColor = color;
+        this.laser.endColor = color;
 
-        CastRay(pos, dir, laser);
+        CastRay(this.pos, this.dir, this.laser);
     }
 
-    void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser){
+    void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser) {
         laserIndices.Add(pos);
 
         Ray ray = new Ray(pos,dir);
         RaycastHit hit;
         int layerMask = 320;
 
-        if (Physics.Raycast(ray, out hit, 30, layerMask)){
+        if (Physics.Raycast(ray, out hit, 100, layerMask)){
             CheckHit(hit,dir,laser);
         }
         else{
@@ -46,7 +50,7 @@ public class LaserBeam{
         CheckEndpoint(hit, dir, laser);
     }
 
-    void UpdateLaser(){
+    void UpdateLaser() {
         int count = 0;
         laser.positionCount = laserIndices.Count;
 
@@ -56,29 +60,47 @@ public class LaserBeam{
         }
     }
 
-    void CheckHit(RaycastHit hitInfo, Vector3 direction, LineRenderer laser)
-    {
+    Vector3 Split(Vector3 norm, Vector3 incident) {
+        norm.Normalize();
+        Vector3 refractedVector;
+
+        refractedVector.y = incident.y;
+        refractedVector.x = 0.707f * (incident.x);
+        refractedVector.z = incident.z;
+        return refractedVector;
+    }
+
+    void CheckHit(RaycastHit hitInfo, Vector3 direction, LineRenderer laser) {
         //Debug.Log("Hit tag: " + hitInfo.collider.gameObject.tag);
         //Debug.Log("Hit layer: " + hitInfo.collider.gameObject.layer);
 
-        // TO DO: update tag for tiles, as tiles increase in number this condition will be hard to maintain
-        if (hitInfo.collider.gameObject.tag == "Vertical" || hitInfo.collider.gameObject.tag == "Horizontal" || hitInfo.collider.gameObject.tag == "Mirror")
-        {
+        if (hitInfo.collider.gameObject.tag == "Vertical" || hitInfo.collider.gameObject.tag == "Horizontal" || hitInfo.collider.gameObject.tag == "Mirror") {
             Vector3 pos = hitInfo.point;
-            Vector3 dir = Vector3.Reflect(direction,hitInfo.normal);
-
-            CastRay(pos,dir,laser);
+            Vector3 dir = Vector3.Reflect(direction, hitInfo.normal);
+            CastRay(pos, dir, laser);
         }
-        else
-        {
+        else if (hitInfo.collider.gameObject.tag == "Splitter") {
+            if (laserObj.name == "Laser Beam 2") {
+                Vector3 pos = hitInfo.point;
+                Vector3 dir = Split(direction, -hitInfo.normal);
+                CastRay(pos, dir, laser);
+            }
+            else {
+                Vector3 pos = hitInfo.point;
+                Vector3 dir = Vector3.Reflect(direction, hitInfo.normal);
+                CastRay(pos, dir, laser);
+            }
+        }
+        else {
             laserIndices.Add(hitInfo.point);
             UpdateLaser();
         }
     }
 
     void CheckEndpoint(RaycastHit hitInfo, Vector3 direction, LineRenderer laser) {
-        if (hitInfo.collider.gameObject.tag == "Endpoint") {
-            endpoint = 1;
+        if (hitInfo.collider.gameObject.name == "Endpoint") {
+            endpoint = true;
+            Debug.Log("Endpoint set to true");
         }
     }
 }
--- Enigma/Assets/Scripts/ShootLaser.cs
+++ Enigma/Assets/Scripts/ShootLaser.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class ShootLaser : MonoBehaviour{
     
     public Material material;
+    public int NumberOfLasers;
     LaserBeam beam;
-    GameObject currentBeam;
-    public GameObject completeLevelUI;
-    public int Endpoints = 1;
+    LaserBeam beam2;
+    string name; //Name of the beam
+    Vector3 pos; //Position of the second beam
+    //GameObject currentBeam;
+    //public GameObject completeLevelUI;
+    //public int Endpoints = 1;
+
+    void Start() {
+        pos = gameObject.transform.position;
+        pos.y = pos.y - 0.01f;
+    }
 
     // Update is called once per frame
     void Update() { 
         Destroy(GameObject.Find("Laser Beam"));
-        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+        //if (NumberOfLasers == 2) {
+            Destroy(GameObject.Find("Laser Beam 2"));
+        //}
 
+        name = "Laser Beam";
+        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material, name, Color.red, false);
+
+        //if (NumberOfLasers == 2) {
+            name = "Laser Beam 2";
+            beam2 = new LaserBeam(pos, gameObject.transform.right, material, name, Color.green, false);
+            //Debug.Log("Laser 2 is Active");
+        //}
         //Debug.Log("Beam endpoint: " + beam.endpoint);
-
-        if (beam.endpoint == Endpoints) {
-            //Debug.Log("Game End");
-            completeLevelUI.SetActive(true);
+        if (beam.endpoint) {
+            Debug.Log("Game End");
+            //completeLevelUI.SetActive(true);
         }
     }
 }
