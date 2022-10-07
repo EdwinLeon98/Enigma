b67193e2ae8c4c86897e5adc6d804440f7e98dfa
Lavanya Narasimhan <lnarasim@usc.edu>
2022-10-07 09:11:33 -0700

    Adding Hints to All Levels

--- Enigma/Assets/Scripts/HintDisplay.cs
+++ Enigma/Assets/Scripts/HintDisplay.cs
@@ -7,13 +7,34 @@ public class HintDisplay : MonoBehaviour
     public GameObject hintTile1;
     public GameObject hintTile2;
     public GameObject hintTile3;
+    public GameObject hintTile4;
+    public GameObject hintTile5;
+    
+public void Start()
+{
+        if(hintTile1!=null)
+        hintTile1.SetActive(false);
+        if(hintTile2!=null)
+        hintTile2.SetActive(false);
+        if(hintTile3!=null)
+        hintTile3.SetActive(false);
+        if(hintTile4!=null)
+            hintTile4.SetActive(false);
+        if(hintTile5!=null)
+        hintTile5.SetActive(false);
+}
 
-    // Update is called once per frame
     public void displayHintTile() {
-        // Debug.Log("Parent Position: " + parent.transform.position);
-        hintTile1.SetActive(true);
-        hintTile2.SetActive(true);
-        hintTile3.SetActive(true);
+        if(hintTile1!=null)
+        hintTile1.SetActive(!hintTile1.activeInHierarchy);
+        if(hintTile2!=null)
+        hintTile2.SetActive(!hintTile2.activeInHierarchy);
+        if(hintTile3!=null)
+        hintTile3.SetActive(!hintTile3.activeInHierarchy);
+        if(hintTile4!=null)        
+        hintTile4.SetActive(!hintTile4.activeInHierarchy);
+        if(hintTile5!=null)        
+        hintTile5.SetActive(!hintTile5.activeInHierarchy);
         
     }
 }
