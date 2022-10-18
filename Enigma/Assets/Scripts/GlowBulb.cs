b7fdf0030071e8ee0ce97c449195a3d3ac9ee100
EdwinLeon98 <edwinleon98@gmail.com>
2022-10-18 10:44:47 -0700

    some changes to glowbulb

--- Enigma/Assets/Scripts/GlowBulb.cs
+++ Enigma/Assets/Scripts/GlowBulb.cs
@@ -8,29 +8,51 @@ public class GlowBulb : MonoBehaviour
     public GameObject lightbulb2;
     Material temp;
     Material temp2;
+    int Time;
+    int oldTime;
+    bool charging;
 
     void Start(){
         temp = lightbulb1.GetComponent<MeshRenderer>().material;
         temp2 = lightbulb2.GetComponent<MeshRenderer>().material;
+        charging = false;
     }
+
     void Update(){
-        if (ShootLaser.nl==1) {
-            if (ShootLaser.end1==true){
-                temp.color = Color.yellow;
+        Time = TimerCounter.mins * 60;
+        Time += TimerCounter.secs * 10;
+        Time += TimerCounter.timer;
+
+        if (ShootLaser.end1 == true && !charging) {
+            oldTime = Time;
+            charging = true;
+        }
+        else if (ShootLaser.end1 == false && charging) {
+            charging = false;
+        }
+
+        if (ShootLaser.nl == 1) {
+            if (ShootLaser.end1 == true){
+                if ((Time - oldTime) <= 2) {
+                    temp.color = new Color(255, 255, 125);
+                }
+                else {
+                    temp.color = new Color(255, 255, 0);
+                }
             }
             else {
                 temp.color = Color.white;
             }
         }
-        else if (ShootLaser.nl==2) {
-            if (ShootLaser.end1 == true){
+        else if (ShootLaser.nl == 2) {
+            if (ShootLaser.end1 == true) {
                 temp.color = Color.yellow;
             }
             else {
                 temp.color = Color.white;
             }
 
-            if (ShootLaser.end2 == true){
+            if (ShootLaser.end2 == true) {
                 temp2.color = Color.yellow;
             }
             else {
