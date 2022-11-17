1edd47feffe6b8df3ca282750c57857740847844
spalivel <spalivel@usc.edu>
2022-11-17 00:31:49 -0800

    Added trigger code to game component

--- Enigma/Assets/Scripts/DisplayTrigger.cs
+++ Enigma/Assets/Scripts/DisplayTrigger.cs
@@ -4,31 +4,26 @@ using UnityEngine;
 
 public class DisplayTrigger : MonoBehaviour
 {
-	// public Transform Spawnpoint;
 	public GameObject gameOver;
 	public GameObject gameCanvasScr;
 
 	void Start()
     {
-    	    	    Debug.Log("Hiii");
 
         gameOver.SetActive(false);
         gameCanvasScr.SetActive(true);
         
     }
 
-    // Start is called before the first frame update
-    void OnTriggerEnter(Collider o)
+    void OnTriggerEnter(Collider other)
     {
-    	    Debug.Log("Is this even being triggered?");
-    	// if(other.tag.Equals("Laser"))
+    	  
            gameOver.SetActive(true);
            gameCanvasScr.SetActive(true);
   }
 
    void OnTriggerExit(Collider other)
     {
-    	Debug.Log("Prasad");
         gameOver.SetActive(false);
         gameCanvasScr.SetActive(true);
     }
