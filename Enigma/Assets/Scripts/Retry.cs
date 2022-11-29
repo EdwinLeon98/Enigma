2273af5cf5d516352c6734d9e0c19661a49601e3
EdwinLeon98 <edwinleon98@gmail.com>
2022-11-29 17:43:38 -0600

    Finished redesign and bug fixes

--- Enigma/Assets/Scripts/Retry.cs
+++ Enigma/Assets/Scripts/Retry.cs
@@ -9,13 +9,14 @@ public class Retry : MonoBehaviour
     // Update is called once per frame
     public void RetryLevel(string levelName)
     {
-         SceneManager.LoadScene(levelName);
-         MovePrototype2.numberOfMoves=0;
-         TimerCounter.mins=0;
-         TimerCounter.secs=0;
-         TimerCounter.timer=0;
-         PauseGame.unpause=true;
-         Metrics.IncrementNumRetries();
-         Metrics.ResetMetricsExceptRetry();
+        SceneManager.LoadScene(levelName);
+        MovePrototype2.numberOfMoves=0;
+        TimerCounter.mins=0;
+        TimerCounter.secs=0;
+        TimerCounter.timer=0;
+        PauseGame.unpause=true;
+        Time.timeScale = 1;
+        Metrics.IncrementNumRetries();
+        Metrics.ResetMetricsExceptRetry();
     }
 }
